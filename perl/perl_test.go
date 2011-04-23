1d38e01752b2b717d5b2b2d9278717b5b284f5f8
Brad Fitzpatrick <brad@danga.com>
2011-04-22 21:57:25 -0700

    more tests

--- perl/perl_test.go
+++ perl/perl_test.go
@@ -13,4 +13,9 @@ func TestPerl(t *testing.T) {
 	if e, g := 13, perl.EvalInt("5 + 8"); e != g {
 		t.Errorf("5 + 8 expected %d; got %d", e, g)
 	}
+
+	perl.Eval("$foo = 123;")
+	if e, g := 123, perl.EvalInt("$foo"); e != g {
+		t.Errorf("$foo expected %d; got %d", e, g)
+	}
 }

749abe12d216c74038dea8698a35f1918c4b99e9
Quentin Smith <quentin@google.com>
2016-05-04 11:25:47 -0700

    Update CFLAGS and LDFLAGS to build on both Darwin and Linux

--- perl/campher.go
+++ perl/campher.go
@@ -27,8 +27,12 @@ limitations under the License.
 package perl
 
 /*
-#cgo CFLAGS: -D_REENTRANT -D_GNU_SOURCE -DDEBIAN -fno-strict-aliasing -pipe -fstack-protector -I/usr/local/include -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64 -I/System/Library/Perl/5.18/darwin-thread-multi-2level/CORE -I/usr/lib/perl/5.10/CORE
-#cgo LDFLAGS:  -fstack-protector -L/usr/local/lib  -L/System/Library/Perl/5.18/darwin-thread-multi-2level/CORE -L/usr/lib/perl/5.10/CORE -lperl -ldl -lm -lpthread -lc
+#cgo CFLAGS: -D_REENTRANT -D_GNU_SOURCE -DDEBIAN -fno-strict-aliasing -pipe -fstack-protector -I/usr/local/include -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64
+#cgo LDFLAGS:  -fstack-protector -L/usr/local/lib -lperl -ldl -lm -lpthread -lc
+#cgo !darwin CFLAGS: -I/usr/lib/perl/5.10/CORE
+#cgo !darwin LDFLAGS: -Wl,-E -lcrypt -L/usr/lib/perl/5.10/CORE
+#cgo darwin CFLAGS: -I/System/Library/Perl/5.18/darwin-thread-multi-2level/CORE
+#cgo darwin LDFLAGS: -L/System/Library/Perl/5.18/darwin-thread-multi-2level/CORE
 #include <EXTERN.h>
 #include <perl.h>
 #include "campher.h"
